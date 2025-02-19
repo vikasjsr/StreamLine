2f76cce8e8e3119fcc6dbda9bb02c99edfdbeeb1
Arsh <arshagrawal3105@gmail.com>
2025-02-19 09:29:02 +0530

    SL

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react"
+import Navbar from "./Components/Navbar"
+import Newsboard from "./Components/Newsboard"
+
+const App = () => {
+  const [category , setCategory] = useState("general");
+  return (
+    <div>
+      <Navbar setCategory={setCategory}/>
+      <Newsboard category={category}/>
+    
+    </div>
+  )
+}
+
+export default App
+    
